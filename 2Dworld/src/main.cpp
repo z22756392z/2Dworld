66a71084a472445bd4d386444edc36339aac9117
z22756392z <z22756392z10@gmail.com>
2021-06-30 15:43:29 +0800

    Add project files.

---
+++ 2Dworld/src/main.cpp
@@ -0,0 +1,45 @@
+#pragma once
+#include <glfw/glfw3.h>
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glBegin(GL_TRIANGLES);
+        /* 2f mean specify two component*/
+        /*By default projection matrix in opengl is between -1 and 1 in every dimension, in every Axis*/
+        glVertex2f(1,1);
+        glVertex2f(1, 0);
+        glVertex2f(0, 0);
+        glEnd();
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
